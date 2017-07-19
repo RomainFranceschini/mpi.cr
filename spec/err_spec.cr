6ae5f75cf79921c8f44899ba5f848b11b37c4ac5
Romain Franceschini <franceschini.romain@gmail.com>
2017-07-19 17:47:22 +0200

    fix spec

--- spec/err_spec.cr
+++ spec/err_spec.cr
@@ -1,23 +1,36 @@
 require "./spec_helper"
 
-MPI.init
-
 describe "errors" do
   describe "err?" do
     it "does nothing if code is successful" do
-      MPI.err?(LibMPI::SUCCESS).should be_nil
+      MPI.init { MPI.err?(LibMPI::SUCCESS).should be_nil }
     end
 
     it "raises appropriate exception for known errors" do
-      expect_raises MPI::InvalidBufferError do
-        MPI.err?(LibMPI::ERR_BUFFER)
-      end
+      MPI.init do
+        expect_raises MPI::Errors::InvalidBufferError do
+          MPI.err?(LibMPI::ERR_BUFFER)
+        end
 
-      expect_raises MPI::NoMemoryError do
-        MPI.err?(LibMPI::ERR_NO_MEM)
+        expect_raises MPI::Errors::NoMemoryError do
+          MPI.err?(LibMPI::ERR_NO_MEM)
+        end
+      end
+    end
+  end
+
+  describe "MPIError" do
+    describe "#message" do
+      it "fetch associated message from MPI library" do
+        MPI.init do
+          err = MPI::Errors::UnknownError.new
+          err.message.should eq("Unknown error")
+
+          err = MPI::Errors::InvalidBufferError.new
+          err.message.should eq("Invalid buffer pointer")
+        end
       end
     end
   end
 end
 
-MPI.finalize
