a26797d23155864c1af201eb598ba243678fb006
Romain Franceschini <franceschini.romain@gmail.com>
2018-02-15 17:20:05 +0100

    hopefully fix immediate.cr example

--- examples/immediate.cr
+++ examples/immediate.cr
@@ -9,19 +9,16 @@ MPI.init do |universe|
   sreq = world.this_process.immediate_send(x)
   rreq = world.any_process.immediate_receive(pointerof(y), 1)
   rreq.wait
-
   loop do
-    if status = sreq.status
-      break
-    end
+    break if sreq.completed?
   end
-
   assert x == y
 
   y = 0.0
   rreq = world.any_process.immediate_receive(pointerof(y), 1)
-  sreq = world.this_process.immediate_send(x)
-
+  sreq = world.this_process.immediate_ready_send(x)
+  rreq.wait
+  sreq.wait
   assert x == y
 
   assert world.any_process.immediate_probe == nil
@@ -30,16 +27,32 @@ MPI.init do |universe|
   y = 0.0
   sreq = world.this_process.immediate_synchronous_send(x)
   if preq = world.any_process.immediate_matched_probe
-    msg, _ = preq
+    msg, status = preq
     rreq = msg.immediate_matched_receive(pointerof(y), 1)
-    assert x == y
+    sreq.wait
+    rreq.wait
   else
     raise "immediate matched probe should not be nil"
   end
+  assert x == y
 
   future = world.any_process.immediate_receive(Float64)
   world.this_process.send(x)
   y, _ = future.get
-
   assert x == y
+
+  future = world.any_process.immediate_receive(Float64)
+  assert future.try == nil
+  world.this_process.send(x)
+  loop do
+    if tuple = future.try
+      msg, _ = tuple
+      assert x == msg
+      break
+    end
+  end
+
+  sreq = world.this_process.immediate_send(x)
+  sreq.cancel
+  sreq.wait
 end
